b1e903ceaa9e4d9394e97db393b5cf246499b30a
JOrtegaCeron <jortegaceron@gmail.com>
2026-07-01 14:58:35 +0000

    sesion-04: sec-4 ufuncs

--- src/numpy_utils.py
+++ src/numpy_utils.py
@@ -311,7 +311,7 @@ def calcular_variacion_absoluta(valores_actuales, valores_anteriores):
         -> array([200000., 100000.,      0.])
     """
     # TODO: usa np.abs(valores_actuales - valores_anteriores)
-    pass
+    return np.abs(valores_actuales - valores_anteriores)
 
 
 def normalizar_valores(arr):
@@ -334,7 +334,9 @@ def normalizar_valores(arr):
     """
     # TODO: calcula minimo = arr.min(), maximo = arr.max()
     #       retorna (arr - minimo) / (maximo - minimo)
-    pass
+    minimo = arr.min()
+    maximo = arr.max()
+    return (arr - minimo) / (maximo - minimo)
 
 
 def aplicar_raiz_cuadrada(arr):
@@ -354,7 +356,7 @@ def aplicar_raiz_cuadrada(arr):
         -> array([  0.      , 316.22...,  632.45...,  948.68...])
     """
     # TODO: usa np.sqrt(arr)
-    pass
+    return np.sqrt(arr)
 
 
 # ===========================================================================
